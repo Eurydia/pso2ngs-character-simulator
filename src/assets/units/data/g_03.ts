3edafc6d878c7fa67ac43549eba23fcda17cf5b1
Euryn <crossdeltavi@gmail.com>
2022-12-09 10:19:29 +0100

    added `Renaissa Armor`

--- src/assets/units/data/g_03.ts
+++ src/assets/units/data/g_03.ts
@@ -23,6 +23,7 @@ const makeUnitThree = (
 // -------------------------
 data.push(
   makeUnitThree("Theseus Armor", {
+    [StatEnum.CORE_DEFENSE]: 9,
     [StatEnum.CORE_HP]: 10,
     [StatEnum.CORE_PP]: 1,
   }),
@@ -30,8 +31,20 @@ data.push(
 
 data.push(
   makeUnitThree("Gold Primm Armor", {
+    [StatEnum.CORE_DEFENSE]: 9,
     [StatEnum.CORE_HP]: 10,
     [StatEnum.CORE_PP]: 1,
+  }),
+);
+
+data.push(
+  makeUnitThree("Renaissa Armor", {
+    [StatEnum.CORE_DEFENSE]: 10,
+    [StatEnum.CORE_HP]: 10,
+    [StatEnum.CORE_PP]: 2,
+    [StatEnum.WEAPON_MELEE]: 1.01,
+    [StatEnum.WEAPON_RANGED]: 1.01,
+    [StatEnum.WEAPON_TECHNIQUE]: 1.01,
   }),
 );
 
