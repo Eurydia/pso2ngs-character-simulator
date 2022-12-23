21caed4407df6a32569842f30121db8bc99b836e
Euryn <crossdeltavi@gmail.com>
2022-12-23 21:29:08 +0100

    Update helper.tsx

--- src/components/AutocompleteWeapon/helper.tsx
+++ src/components/AutocompleteWeapon/helper.tsx
@@ -3,32 +3,35 @@ import { matchSorter } from "match-sorter";
 
 import { Weapon } from "../../assets";
 
-const collectTerms = (value: string): string[] => {
+const extractTerms = (value: string): string[] => {
+  const items = value.split(" ");
+
   const terms: string[] = [];
 
-  for (const item of value.split(" ")) {
+  for (const item of items) {
     const item_trimmed = item.trim();
 
-    if (item_trimmed.length === 0) {
+    if (item_trimmed === "") {
       continue;
     }
 
     terms.push(item_trimmed);
   }
+
   return terms;
 };
 
-const reducer = (options: Weapon[], term: string): Weapon[] => {
+const termReducer = (options: Weapon[], term: string): Weapon[] => {
   return matchSorter(options, term, {
-    keys: [(item) => item.name, (item) => item.group],
+    keys: [(item) => item.name, (item) => item.rarity],
   });
 };
 
-const collectOptions = (
+const sieveOptions = (
   options: Weapon[],
   terms: string[],
 ): Weapon[] => {
-  return terms.reduceRight(reducer, options);
+  return terms.reduceRight(termReducer, options);
 };
 
 export const filterOptions = (
@@ -37,8 +40,8 @@ export const filterOptions = (
   size: number = 16,
 ) => {
   const value: string = state.inputValue;
-  const terms: string[] = collectTerms(value);
-  const filtered_options: Weapon[] = collectOptions(
+  const terms: string[] = extractTerms(value);
+  const filtered_options: Weapon[] = sieveOptions(
     options,
     terms,
   ).slice(0, size);
