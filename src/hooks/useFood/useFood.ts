0c617d255172ca13af217b4e72ddd8db49cfa38f
Euryn <crossdeltavi@gmail.com>
2022-12-29 18:39:25 +0100

    create template for `useFood` hook

--- src/hooks/useFood/useFood.ts
+++ src/hooks/useFood/useFood.ts
@@ -1,6 +1,29 @@
 import { useState } from "react";
 import { Food } from "../../assets";
 
-export const useFood = () => {
+export const useFood = (): [
+  Food[],
+  (item: Food | null, index: number) => void,
+] => {
   const [value, _setValue] = useState<Food[]>([]);
+
+  const setValue = (next_value: Food | null, index: number) => {
+    if (index < 0 || value.length <= index) {
+      return;
+    }
+
+    _setValue((prev) => {
+      const next = [...prev];
+
+      if (next_value === null) {
+        next.splice(index, 1);
+        return next;
+      }
+
+      next[index] = next_value!;
+      return next;
+    });
+  };
+
+  return [value, setValue];
 };
