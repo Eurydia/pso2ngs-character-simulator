306366e171fb3b56ed815971c828f0a248fd3f38
Euryn <crossdeltavi@gmail.com>
2022-12-31 14:45:03 +0100

    mockup for Context object

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import { FoodAttributeEnum, FoodCategoryEnum } from "./assets";
-
 export type SummaryEquipment = {
   equipment: string | null;
   fixa: string | null;
@@ -10,3 +8,30 @@ export type SummaryFood = {
   level: number;
   label: string;
 };
+
+type TargetContext = Partial<{
+  isBoss: boolean;
+  isNonBoss: boolean;
+
+  isBlightMarked: boolean;
+  isWeakSpot: boolean;
+
+  isWeakToLightning: boolean;
+  isWeakToIce: boolean;
+  isWeakToFire: boolean;
+  isWeakToWind: boolean;
+  isWeakToLight: boolean;
+  isWeakToDark: boolean;
+}>;
+
+type TimeContext = Partial<{
+  isDayTime: boolean;
+  isNightTime: boolean;
+}>;
+
+export type CharacterActionContext = Partial<{
+  isIdle: boolean;
+  isAttacking: boolean;
+  target: TargetContext;
+  time: TimeContext;
+}>;
