13cffce8639aa801141c8867cc1e2bfac81f68d2
Euryn <crossdeltavi@gmail.com>
2023-01-02 21:00:40 +0100

    refactored `unit`

--- src/assets/fixas/data/g_unit.ts
+++ src/assets/fixas/data/g_unit.ts
@@ -2,63 +2,63 @@ import { StatEnum, statObject } from "../../stat";
 import { fixa, Fixa } from "../fixas";
 import { GroupEnumFixa } from "../groupEnum";
 
-export const g_unit: Fixa[] = [];
+export const G_UNIT: Fixa[] = [];
 
 const makeFixaUnit = (
   name: string,
   level: number,
   stat: Partial<{ [K in StatEnum]: number }>,
 ) => {
-  return fixa(name, level, GroupEnumFixa.UNIT, (_) =>
-    statObject(stat),
-  );
+  return fixa(name, level, GroupEnumFixa.UNIT, (_) => {
+    return statObject(stat);
+  });
 };
 
 // --------------------------------------
 (() => {
-  const data_value = [1.01, 1.02, 1.03, 1.035, 1.04];
+  const DATA_DAMAGE_RES: number[] = [1.01, 1.02, 1.03, 1.035, 1.04];
 
-  data_value.forEach((value, level_index) => {
-    g_unit.push(
-      makeFixaUnit("Fixa Guard", level_index + 1, {
-        [StatEnum.ADV_DEF_DAMAGE_RES]: value,
-      }),
-    );
+  DATA_DAMAGE_RES.forEach((damage_res, level_index) => {
+    const level: number = level_index + 1;
+    const unit_fixa: Fixa = makeFixaUnit("Fixa Guard", level, {
+      [StatEnum.ADV_DEF_DAMAGE_RES]: damage_res,
+    });
+    G_UNIT.push(unit_fixa);
   });
 })();
 
 (() => {
-  const data_value = [0.99, 0.98, 0.97, 0.965, 0.96];
+  const DATA_PP_USAGE: number[] = [0.99, 0.98, 0.97, 0.965, 0.96];
 
-  data_value.forEach((value, level_index) => {
-    g_unit.push(
-      makeFixaUnit("Fixa Performa", level_index + 1, {
-        [StatEnum.ADV_PP_USAGE]: value,
-      }),
-    );
+  DATA_PP_USAGE.forEach((pp_usage, level_index) => {
+    const level: number = level_index + 1;
+    const unit_fixa: Fixa = makeFixaUnit("Fixa Performa", level, {
+      [StatEnum.ADV_PP_USAGE]: pp_usage,
+    });
+    G_UNIT.push(unit_fixa);
   });
 })();
 
 (() => {
-  const data_value = [1.02, 1.03, 1.04, 1.045, 1.05];
+  const DATA_PP_RECOVERY: number[] = [1.02, 1.03, 1.04, 1.045, 1.05];
 
-  data_value.forEach((value, level_index) => {
-    g_unit.push(
-      makeFixaUnit("Fixa Natura", level_index + 1, {
-        [StatEnum.ADV_PP_NATURAL_RECOVERY]: value,
-      }),
-    );
+  DATA_PP_RECOVERY.forEach((pp_recovery, level_index) => {
+    const level: number = level_index + 1;
+    const unit_fixa: Fixa = makeFixaUnit("Fixa Natura", level, {
+      [StatEnum.ADV_PP_NATURAL_RECOVERY]: pp_recovery,
+    });
+    G_UNIT.push(unit_fixa);
   });
 })();
 
 (() => {
-  const data_value = [1.02, 1.03, 1.04, 1.045, 1.05];
+  const DATA_PP_RECOVERY: number[] = [1.02, 1.03, 1.04, 1.045, 1.05];
 
-  data_value.forEach((value, level_index) => {
-    g_unit.push(
-      makeFixaUnit("Fixa Enthusia", level_index + 1, {
-        [StatEnum.ADV_PP_ACTIVE_RECOVERY]: value,
-      }),
-    );
+  DATA_PP_RECOVERY.forEach((pp_recovery, level_index) => {
+    const level: number = level_index + 1;
+    const fixa_unit: Fixa = makeFixaUnit("Fixa Enthusia", level, {
+      [StatEnum.ADV_PP_ACTIVE_RECOVERY]: pp_recovery,
+    });
+    G_UNIT.push(fixa_unit);
   });
 })();
--- src/assets/fixas/data/index.ts
+++ src/assets/fixas/data/index.ts
@@ -1,5 +1,5 @@
 import { Fixa } from "../fixas";
-import { g_unit } from "./g_unit";
+import { G_UNIT } from "./g_unit";
 import { g_weapon } from "./g_weapon";
 
-export const AssetFixas: Fixa[] = [...g_unit, ...g_weapon];
+export const AssetFixas: Fixa[] = [...G_UNIT, ...g_weapon];
