350e8d6be084ca74a8f1fadc79b3b8d961370957
Euryn <crossdeltavi@gmail.com>
2023-01-02 21:02:40 +0100

    refactored `weapon`

--- src/assets/fixas/data/g_weapon.ts
+++ src/assets/fixas/data/g_weapon.ts
@@ -2,51 +2,51 @@ import { StatEnum, statObject } from "../../stat";
 import { fixa, Fixa } from "../fixas";
 import { GroupEnumFixa } from "../groupEnum";
 
-export const g_weapon: Fixa[] = [];
+export const G_WEAPON: Fixa[] = [];
 
 const makeFixaWeapon = (
   name: string,
   level: number,
   stat: Partial<{ [K in StatEnum]: number }>,
 ) => {
-  return fixa(name, level, GroupEnumFixa.WEAPON, (_) =>
-    statObject(stat),
-  );
+  return fixa(name, level, GroupEnumFixa.WEAPON, (_) => {
+    return statObject(stat);
+  });
 };
 
 // --------------------------------------
 (() => {
-  const data_value = [1.02, 1.03, 1.04, 1.045, 1.05];
+  const DATA_DAMAGE_UP: number[] = [1.02, 1.03, 1.04, 1.045, 1.05];
 
-  data_value.forEach((value, level_index) => {
-    g_weapon.push(
-      makeFixaWeapon("Fixa Attack", level_index + 1, {
-        [StatEnum.ADV_OFF_DAMAGE_UP]: value,
-      }),
-    );
+  DATA_DAMAGE_UP.forEach((damage_up, level_index) => {
+    const level: number = level_index + 1;
+    const weapon_fixa: Fixa = makeFixaWeapon("Fixa Attack", level, {
+      [StatEnum.ADV_OFF_DAMAGE_UP]: damage_up,
+    });
+    G_WEAPON.push(weapon_fixa);
   });
 })();
 
 (() => {
-  const data_value = [0.05, 0.08, 0.1, 0.12, 0.13];
+  const DATA_CRIT_CHANCE: number[] = [0.05, 0.08, 0.1, 0.12, 0.13];
 
-  data_value.forEach((value, level_index) => {
-    g_weapon.push(
-      makeFixaWeapon("Fixa Fatale", level_index + 1, {
-        [StatEnum.ADV_OFF_CRIT_CHANCE]: value,
-      }),
-    );
+  DATA_CRIT_CHANCE.forEach((crit_chance, level_index) => {
+    const level: number = level_index + 1;
+    const weapon_fixa: Fixa = makeFixaWeapon("Fixa Fatale", level, {
+      [StatEnum.ADV_OFF_CRIT_CHANCE]: crit_chance,
+    });
+    G_WEAPON.push(weapon_fixa);
   });
 })();
 
 (() => {
-  const data_value = [1.05, 1.09, 1.12, 1.14, 1.15];
+  const DATA_CRIT_DAMAGE: number[] = [1.05, 1.09, 1.12, 1.14, 1.15];
 
-  data_value.forEach((value, level_index) => {
-    g_weapon.push(
-      makeFixaWeapon("Fixa Termina", level_index + 1, {
-        [StatEnum.ADV_OFF_CRIT_DAMAGE]: value,
-      }),
-    );
+  DATA_CRIT_DAMAGE.forEach((crit_damage, level_index) => {
+    const level: number = level_index + 1;
+    const weapon_fixa: Fixa = makeFixaWeapon("Fixa Termina", level, {
+      [StatEnum.ADV_OFF_CRIT_DAMAGE]: crit_damage,
+    });
+    G_WEAPON.push(weapon_fixa);
   });
 })();
--- src/assets/fixas/data/index.ts
+++ src/assets/fixas/data/index.ts
@@ -1,5 +1,5 @@
 import { Fixa } from "../fixas";
 import { G_UNIT } from "./g_unit";
-import { g_weapon } from "./g_weapon";
+import { G_WEAPON } from "./g_weapon";
 
-export const AssetFixas: Fixa[] = [...G_UNIT, ...g_weapon];
+export const AssetFixas: Fixa[] = [...G_UNIT, ...G_WEAPON];
