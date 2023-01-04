54401073adcd5884b9b8fa65afe853c1e3ea0feb
Euryn <crossdeltavi@gmail.com>
2023-01-04 20:23:49 +0100

    Update FormFood.tsx

--- src/components/FormFood/FormFood.tsx
+++ src/components/FormFood/FormFood.tsx
@@ -1,9 +1,8 @@
-import { FC, useEffect, useState, useMemo } from "react";
+import { FC, useState, useMemo } from "react";
 import { Box, Button, Stack } from "@mui/material";
 import { Add } from "@mui/icons-material";
 
 import { ActionContext, Food, StatObject } from "../../assets";
-import { useFood } from "../../hooks";
 
 import { AutocompleteFood } from "../AutocompleteFood";
 
@@ -15,38 +14,34 @@ import { StatView } from "../StatView";
 const CONTEXT: ActionContext = { location: { kvaris: true } };
 
 type FormFoodProps = {
-  storage_key: string;
-  onStatChange: (stat: StatObject) => void;
+  items: Food[];
+  onItemAdd: (index: number, item: Food) => void;
+  onItemRemove: (index: number) => void;
 };
 export const FormFood: FC<FormFoodProps> = (props) => {
-  const { onStatChange, storage_key } = props;
+  const { items, onItemAdd, onItemRemove } = props;
 
-  const [items, addItem, removeItem] = useFood(storage_key);
   const [selected, setSelected] = useState<Food | null>(null);
 
   const handleAdd = () => {
     if (selected === null) {
       return;
     }
-    addItem(0, selected);
+    onItemAdd(0, selected);
     setSelected(null);
   };
 
   const handleCopy = (index: number, item: Food) => {
-    addItem(index, item);
+    onItemAdd(index, item);
   };
 
   const handleRemove = (index: number) => {
-    removeItem(index);
+    onItemRemove(index);
   };
 
   const stat = useMemo((): StatObject => {
     return createStat(CONTEXT, items);
   }, [items]);
-
-  useEffect(() => {
-    onStatChange(stat);
-  }, [stat]);
 
   return (
     <FormBase
