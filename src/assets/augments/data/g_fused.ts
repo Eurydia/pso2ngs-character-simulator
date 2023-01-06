75c752e9b345585f992ae511982c8caadcfd073f
Euryn <crossdeltavi@gmail.com>
2023-01-06 09:47:16 +0100

    Update g_fused.ts

--- src/assets/augments/data/g_fused.ts
+++ src/assets/augments/data/g_fused.ts
@@ -1,4 +1,5 @@
-import { StatEnum, statObject } from "../../stat";
+import { ActionContext } from "../../context";
+import { StatEnum, StatObject, statObject } from "../../stat";
 import { augment, Augment } from "../augment";
 import { GroupEnumAugment } from "../groupEnum";
 
@@ -7,16 +8,14 @@ export const G_FUSED: Augment[] = [];
 const makeAugmentFused = (
   name: string,
   level: number,
-  stat: Partial<{ [K in StatEnum]: number }>,
+  getAwareStatObject: (ctx: ActionContext) => StatObject,
 ): Augment => {
   return augment(
     name,
     level,
     GroupEnumAugment.FUSED,
     [GroupEnumAugment.BASIC, GroupEnumAugment.FUSED],
-    (_) => {
-      return statObject(stat);
-    },
+    getAwareStatObject,
   );
 };
 
@@ -28,41 +27,47 @@ const makeAugmentFused = (
     ["Precision", StatEnum.WEAPON_RANGED],
     ["Technique", StatEnum.WEAPON_TECHNIQUE],
   ];
-
   const WEAPON_UP = 1.02;
-
   for (const entry of DATA_ENTRY) {
     const [name, stat_weapon_up] = entry;
 
     G_FUSED.push(
-      makeAugmentFused(`Sta ${name}`, 0, {
-        [StatEnum.CORE_BP]: 8,
-        [StatEnum.CORE_HP]: 15,
-        [stat_weapon_up]: WEAPON_UP,
+      makeAugmentFused(`Sta ${name}`, 0, (_) => {
+        return statObject({
+          [StatEnum.CORE_BP]: 8,
+          [StatEnum.CORE_HP]: 15,
+          [stat_weapon_up]: WEAPON_UP,
+        });
       }),
     );
 
     G_FUSED.push(
-      makeAugmentFused(`Spi ${name}`, 0, {
-        [StatEnum.CORE_BP]: 8,
-        [StatEnum.CORE_PP]: 5,
-        [stat_weapon_up]: WEAPON_UP,
+      makeAugmentFused(`Spi ${name}`, 0, (_) => {
+        return statObject({
+          [StatEnum.CORE_BP]: 8,
+          [StatEnum.CORE_PP]: 5,
+          [stat_weapon_up]: WEAPON_UP,
+        });
       }),
     );
 
     G_FUSED.push(
-      makeAugmentFused(`Deft ${name}`, 0, {
-        [StatEnum.CORE_BP]: 8,
-        [stat_weapon_up]: WEAPON_UP,
-        [StatEnum.ADV_OFF_FLOOR]: 1.02,
+      makeAugmentFused(`Deft ${name}`, 0, (_) => {
+        return statObject({
+          [StatEnum.CORE_BP]: 8,
+          [stat_weapon_up]: WEAPON_UP,
+          [StatEnum.ADV_OFF_FLOOR]: 1.02,
+        });
       }),
     );
 
     G_FUSED.push(
-      makeAugmentFused(`Gua ${name}`, 0, {
-        [StatEnum.CORE_BP]: 8,
-        [stat_weapon_up]: WEAPON_UP,
-        [StatEnum.ADV_DEF_DAMAGE_RES]: 1.02,
+      makeAugmentFused(`Gua ${name}`, 0, (_) => {
+        return statObject({
+          [StatEnum.CORE_BP]: 8,
+          [stat_weapon_up]: WEAPON_UP,
+          [StatEnum.ADV_DEF_DAMAGE_RES]: 1.02,
+        });
       }),
     );
   }
