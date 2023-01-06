62c3345951cbe31ba10844188d97e157d5f88856
Euryn <crossdeltavi@gmail.com>
2023-01-06 10:13:59 +0100

    Update g_secreta.ts

--- src/assets/augments/data/g_secreta.ts
+++ src/assets/augments/data/g_secreta.ts
@@ -1,4 +1,5 @@
-import { StatEnum, statObject } from "../../stat";
+import { ActionContext } from "../../context";
+import { StatEnum, StatObject, statObject } from "../../stat";
 import { augment, Augment } from "../augment";
 import { GroupEnumAugment } from "../groupEnum";
 
@@ -7,16 +8,14 @@ export const G_SECRETA: Augment[] = [];
 const makeAugmentSecreta = (
   name: string,
   level: number,
-  stat: Partial<{ [K in StatEnum]: number }>,
+  getAwareStatObject: (ctx: ActionContext) => StatObject,
 ): Augment => {
   return augment(
     name,
     level,
     GroupEnumAugment.SECRETA,
     [GroupEnumAugment.SECRETA],
-    (_) => {
-      return statObject(stat);
-    },
+    getAwareStatObject,
   );
 };
 
@@ -28,18 +27,14 @@ const makeAugmentSecreta = (
   const DATA_WEAPON_UP: number[] = [1.01, 1.015, 1.02, 1.025];
   const DATA_FLOOR_UP: number[] = [1.01, 1.015, 1.02, 1.025];
   const DATA_DAMAGE_RES: number[] = [0.985, 0.985, 0.985, 0.985];
-
   DATA_BP.forEach((bp, level_index) => {
     const level: number = level_index + 1;
     const hp: number = DATA_HP[level_index];
     const weapon_up: number = DATA_WEAPON_UP[level_index];
     const floor_up: number = DATA_FLOOR_UP[level_index];
     const damage_res: number = DATA_DAMAGE_RES[level_index];
-
-    const secreta_augment: Augment = makeAugmentSecreta(
-      "Alts Secreta",
-      level,
-      {
+    const _getter = (_: ActionContext): StatObject => {
+      return statObject({
         [StatEnum.CORE_BP]: bp,
         [StatEnum.CORE_HP]: hp,
         [StatEnum.WEAPON_MELEE]: weapon_up,
@@ -47,9 +42,13 @@ const makeAugmentSecreta = (
         [StatEnum.WEAPON_TECHNIQUE]: weapon_up,
         [StatEnum.ADV_OFF_FLOOR]: floor_up,
         [StatEnum.ADV_DEF_DAMAGE_RES]: damage_res,
-      },
+      });
+    };
+    const secreta_augment: Augment = makeAugmentSecreta(
+      "Alts Secreta",
+      level,
+      _getter,
     );
-
     G_SECRETA.push(secreta_augment);
   });
 })();
