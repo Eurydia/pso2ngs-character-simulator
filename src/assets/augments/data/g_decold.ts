ed3cb13e67acc8808159111a485674d5f22e7cbc
Euryn <crossdeltavi@gmail.com>
2023-01-06 09:29:53 +0100

    Update g_decold.ts

--- src/assets/augments/data/g_decold.ts
+++ src/assets/augments/data/g_decold.ts
@@ -1,4 +1,5 @@
-import { StatEnum, statObject } from "../../stat";
+import { ActionContext } from "../../context";
+import { StatEnum, StatObject, statObject } from "../../stat";
 
 import { augment, Augment } from "../augment";
 import { GroupEnumAugment } from "../groupEnum";
@@ -8,16 +9,14 @@ export const G_DECOLD: Augment[] = [];
 const makeAugmentDecold = (
   name: string,
   level: number,
-  stat: Partial<{ [K in StatEnum]: number }>,
+  getAwareStatObject: (ctx: ActionContext) => StatObject,
 ): Augment => {
   return augment(
     name,
     level,
     GroupEnumAugment.DECOLD,
     [GroupEnumAugment.DECOLD],
-    (_) => {
-      return statObject(stat);
-    },
+    getAwareStatObject,
   );
 };
 
@@ -26,20 +25,20 @@ const makeAugmentDecold = (
 (() => {
   const DATA_BP: number[] = [1, 3, 5];
   const DATA_HARSH_COLD: number[] = [0.05, 0.15, 0.25];
-
   DATA_BP.forEach((bp, level_index) => {
     const level: number = level_index + 1;
     const harsh_cold: number = DATA_HARSH_COLD[level_index];
-
+    const _getter = (_: ActionContext) => {
+      return statObject({
+        [StatEnum.CORE_BP]: bp,
+        [StatEnum.HARSH_COLD]: harsh_cold,
+      });
+    };
     const decold_augment: Augment = makeAugmentDecold(
       "Decold Standard",
       level,
-      {
-        [StatEnum.CORE_BP]: bp,
-        [StatEnum.HARSH_COLD]: harsh_cold,
-      },
+      _getter,
     );
-
     G_DECOLD.push(decold_augment);
   });
 })();
@@ -52,23 +51,20 @@ const makeAugmentDecold = (
     ["Precision", StatEnum.WEAPON_RANGED],
     ["Technique", StatEnum.WEAPON_TECHNIQUE],
   ];
-
-  const WEAPON_UP: number = 1.025;
-  const HARSH_COLD: number = 0.25;
-
   for (const entry of DATA_ENTRY) {
     const [name, stat_weapon_up] = entry;
-
+    const _getter = (_: ActionContext) => {
+      return statObject({
+        [StatEnum.CORE_BP]: 9,
+        [stat_weapon_up]: 1.025,
+        [StatEnum.HARSH_COLD]: 0.25,
+      });
+    };
     const decold_augment: Augment = makeAugmentDecold(
       `Decold ${name}`,
       0,
-      {
-        [StatEnum.CORE_BP]: 9,
-        [stat_weapon_up]: WEAPON_UP,
-        [StatEnum.HARSH_COLD]: HARSH_COLD,
-      },
+      _getter,
     );
-
     G_DECOLD.push(decold_augment);
   }
 })();
