79bf73a522cf005f127282765aaedd03628898eb
Euryn <crossdeltavi@gmail.com>
2023-01-06 09:56:09 +0100

    Update g_domina.ts

--- src/assets/augments/data/g_domina.ts
+++ src/assets/augments/data/g_domina.ts
@@ -22,57 +22,73 @@ const makeAugmentDomina = (
 // --------------------------------------
 // ael
 G_DOMINA.push(
-  makeAugmentDomina("Ael Domina", 0, (_) => {
-    return statObject({
-      [StatEnum.CORE_BP]: 8,
-      [StatEnum.CORE_HP]: 5,
-      [StatEnum.CORE_PP]: 3,
-      [StatEnum.WEAPON_MELEE]: 1.015,
-      [StatEnum.WEAPON_RANGED]: 1.015,
-      [StatEnum.WEAPON_TECHNIQUE]: 1.015,
-    });
-  }),
+  makeAugmentDomina(
+    "Ael Domina",
+    0,
+    (_: ActionContext): StatObject => {
+      return statObject({
+        [StatEnum.CORE_BP]: 8,
+        [StatEnum.CORE_HP]: 5,
+        [StatEnum.CORE_PP]: 3,
+        [StatEnum.WEAPON_MELEE]: 1.015,
+        [StatEnum.WEAPON_RANGED]: 1.015,
+        [StatEnum.WEAPON_TECHNIQUE]: 1.015,
+      });
+    },
+  ),
 );
 
 // --------------------------------------
 // ret
 G_DOMINA.push(
-  makeAugmentDomina("Ret Domina", 0, (_) => {
-    return statObject({
-      [StatEnum.CORE_BP]: 10,
-      [StatEnum.CORE_HP]: 15,
-      [StatEnum.WEAPON_MELEE]: 1.015,
-      [StatEnum.WEAPON_RANGED]: 1.015,
-      [StatEnum.WEAPON_TECHNIQUE]: 1.015,
-    });
-  }),
+  makeAugmentDomina(
+    "Ret Domina",
+    0,
+    (_: ActionContext): StatObject => {
+      return statObject({
+        [StatEnum.CORE_BP]: 10,
+        [StatEnum.CORE_HP]: 15,
+        [StatEnum.WEAPON_MELEE]: 1.015,
+        [StatEnum.WEAPON_RANGED]: 1.015,
+        [StatEnum.WEAPON_TECHNIQUE]: 1.015,
+      });
+    },
+  ),
 );
 
 // --------------------------------------
 // kvar
 G_DOMINA.push(
-  makeAugmentDomina("Kvar Domina", 0, (_) => {
-    return statObject({
-      [StatEnum.CORE_BP]: 10,
-      [StatEnum.CORE_PP]: 5,
-      [StatEnum.WEAPON_MELEE]: 1.015,
-      [StatEnum.WEAPON_RANGED]: 1.015,
-      [StatEnum.WEAPON_TECHNIQUE]: 1.015,
-      [StatEnum.ADV_DEF_DAMAGE_RES]: 0.98,
-    });
-  }),
+  makeAugmentDomina(
+    "Kvar Domina",
+    0,
+    (_: ActionContext): StatObject => {
+      return statObject({
+        [StatEnum.CORE_BP]: 10,
+        [StatEnum.CORE_PP]: 5,
+        [StatEnum.WEAPON_MELEE]: 1.015,
+        [StatEnum.WEAPON_RANGED]: 1.015,
+        [StatEnum.WEAPON_TECHNIQUE]: 1.015,
+        [StatEnum.ADV_DEF_DAMAGE_RES]: 0.98,
+      });
+    },
+  ),
 );
 
 // --------------------------------------
 // stira
 G_DOMINA.push(
-  makeAugmentDomina("Stira Domina", 0, (_) => {
-    return statObject({
-      [StatEnum.CORE_BP]: 10,
-      [StatEnum.WEAPON_MELEE]: 1.03,
-      [StatEnum.WEAPON_RANGED]: 1.03,
-      [StatEnum.WEAPON_TECHNIQUE]: 1.03,
-      [StatEnum.ADV_DEF_DAMAGE_RES]: 0.98,
-    });
-  }),
+  makeAugmentDomina(
+    "Stira Domina",
+    0,
+    (_: ActionContext): StatObject => {
+      return statObject({
+        [StatEnum.CORE_BP]: 10,
+        [StatEnum.WEAPON_MELEE]: 1.03,
+        [StatEnum.WEAPON_RANGED]: 1.03,
+        [StatEnum.WEAPON_TECHNIQUE]: 1.03,
+        [StatEnum.ADV_DEF_DAMAGE_RES]: 0.98,
+      });
+    },
+  ),
 );
