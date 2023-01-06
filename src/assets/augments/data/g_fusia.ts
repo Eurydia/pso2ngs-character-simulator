dac2769526f540d8c2cbf773d0b187a53ecacf5a
Euryn <crossdeltavi@gmail.com>
2023-01-06 10:02:22 +0100

    Update g_fusia.ts

--- src/assets/augments/data/g_fusia.ts
+++ src/assets/augments/data/g_fusia.ts
@@ -22,12 +22,16 @@ const makeAugmentFusia = (
 // --------------------------------------
 // fusia
 G_FUSIA.push(
-  makeAugmentFusia("Megas Fusia", 0, (_) => {
-    return statObject({
-      [StatEnum.CORE_BP]: 4,
-      [StatEnum.WEAPON_MELEE]: 1.01,
-      [StatEnum.WEAPON_RANGED]: 1.01,
-      [StatEnum.WEAPON_TECHNIQUE]: 1.01,
-    });
-  }),
+  makeAugmentFusia(
+    "Megas Fusia",
+    0,
+    (_: ActionContext): StatObject => {
+      return statObject({
+        [StatEnum.CORE_BP]: 4,
+        [StatEnum.WEAPON_MELEE]: 1.01,
+        [StatEnum.WEAPON_RANGED]: 1.01,
+        [StatEnum.WEAPON_TECHNIQUE]: 1.01,
+      });
+    },
+  ),
 );
