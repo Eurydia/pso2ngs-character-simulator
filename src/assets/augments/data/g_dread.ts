25766c1d1a1730de83653b1ddf6f455cbc7e6d11
Euryn <crossdeltavi@gmail.com>
2023-01-06 09:34:24 +0100

    Update g_dread.ts

--- src/assets/augments/data/g_dread.ts
+++ src/assets/augments/data/g_dread.ts
@@ -1,4 +1,5 @@
-import { StatEnum, statObject } from "../../stat";
+import { ActionContext } from "../../context";
+import { StatEnum, StatObject, statObject } from "../../stat";
 import { augment, Augment } from "../augment";
 import { GroupEnumAugment } from "../groupEnum";
 
@@ -7,16 +8,14 @@ export const G_DREAD: Augment[] = [];
 const makeAugmentDread = (
   name: string,
   level: number,
-  stat: Partial<{ [K in StatEnum]: number }>,
+  getAwareStatObject: (ctx: ActionContext) => StatObject,
 ): Augment => {
   return augment(
     name,
     level,
     GroupEnumAugment.DREAD,
     [GroupEnumAugment.DREAD],
-    (_) => {
-      return statObject(stat);
-    },
+    getAwareStatObject,
   );
 };
 
@@ -28,26 +27,26 @@ const makeAugmentDread = (
   const DATA_PP: number[] = [3, 4, 7, 10];
   const DATA_FLOOR_UP: number[] = [1.01, 1.015, 1.02, 1.06];
   const DATA_DAMAGE_RES: number[] = [1.01, 1.015, 1.02, 1.03];
-
   DATA_BP.forEach((bp, level_index) => {
     const level: number = level_index + 1;
     const hp: number = DATA_HP[level_index];
     const pp: number = DATA_PP[level_index];
     const floor_up: number = DATA_FLOOR_UP[level_index];
     const damage_res: number = DATA_DAMAGE_RES[level_index];
-
-    const dread_augment: Augment = makeAugmentDread(
-      "Dread Keeper",
-      level,
-      {
+    const _getter = (ctx: ActionContext) => {
+      return statObject({
         [StatEnum.CORE_BP]: bp,
         [StatEnum.CORE_HP]: hp,
         [StatEnum.CORE_PP]: pp,
         [StatEnum.ADV_OFF_FLOOR]: floor_up,
         [StatEnum.ADV_DEF_DAMAGE_RES]: damage_res,
-      },
+      });
+    };
+    const dread_augment: Augment = makeAugmentDread(
+      "Dread Keeper",
+      level,
+      _getter,
     );
-
     G_DREAD.push(dread_augment);
   });
 })();
