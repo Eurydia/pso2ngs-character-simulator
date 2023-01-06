1e5045a954994dccc8c487c8a0838769fb6162e0
Euryn <crossdeltavi@gmail.com>
2023-01-06 10:24:26 +0100

    Update g_super.ts

--- src/assets/augments/data/g_super.ts
+++ src/assets/augments/data/g_super.ts
@@ -1,4 +1,5 @@
-import { StatEnum, statObject } from "../../stat";
+import { ActionContext } from "../../context";
+import { StatEnum, StatObject, statObject } from "../../stat";
 import { augment, Augment } from "../augment";
 import { GroupEnumAugment } from "../groupEnum";
 
@@ -7,16 +8,14 @@ export const G_SUPER: Augment[] = [];
 const makeAugmentSuper = (
   name: string,
   level: number,
-  stat: Partial<{ [K in StatEnum]: number }>,
+  getAwareStatObject: (_: ActionContext) => StatObject,
 ): Augment => {
   return augment(
     name,
     level,
     GroupEnumAugment.SUPER,
     [GroupEnumAugment.SUPER],
-    (_) => {
-      return statObject(stat);
-    },
+    getAwareStatObject,
   );
 };
 
@@ -28,17 +27,15 @@ const makeAugmentSuper = (
     ["Precision", StatEnum.WEAPON_RANGED],
     ["Technique", StatEnum.WEAPON_TECHNIQUE],
   ];
-
-  const WEAPON_UP: number = 1.0225;
-
   for (const entry of DATA_ENTRY) {
     const [name, stat_weapon_up] = entry;
-
-    const super_augment: Augment = makeAugmentSuper(name, 1, {
-      [StatEnum.CORE_BP]: 8,
-      [stat_weapon_up]: WEAPON_UP,
-    });
-
+    const _getter = (_: ActionContext): StatObject => {
+      return statObject({
+        [StatEnum.CORE_BP]: 8,
+        [stat_weapon_up]: 1.0225,
+      });
+    };
+    const super_augment: Augment = makeAugmentSuper(name, 1, _getter);
     G_SUPER.push(super_augment);
   }
 })();
@@ -51,26 +48,35 @@ const makeAugmentSuper = (
     ["ra", StatEnum.WEAPON_RANGED],
     ["tech", StatEnum.WEAPON_TECHNIQUE],
   ];
-
   const WEAPON_UP: number = 1.02;
-
   for (const entry of DATA_ENTRY) {
     const [name, stat_weapon_up] = entry;
-
     G_SUPER.push(
-      makeAugmentSuper(`Super Sta${name}`, 1, {
-        [StatEnum.CORE_BP]: 8,
-        [StatEnum.CORE_HP]: 15,
-        [stat_weapon_up]: WEAPON_UP,
-      }),
+      makeAugmentSuper(
+        `Super Sta${name}`,
+        1,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 8,
+            [StatEnum.CORE_HP]: 15,
+            [stat_weapon_up]: WEAPON_UP,
+          });
+        },
+      ),
     );
 
     G_SUPER.push(
-      makeAugmentSuper(`Super Spi${name}`, 1, {
-        [StatEnum.CORE_BP]: 8,
-        [StatEnum.CORE_PP]: 5,
-        [stat_weapon_up]: WEAPON_UP,
-      }),
+      makeAugmentSuper(
+        `Super Spi${name}`,
+        1,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 8,
+            [StatEnum.CORE_PP]: 5,
+            [stat_weapon_up]: WEAPON_UP,
+          });
+        },
+      ),
     );
   }
 })();
