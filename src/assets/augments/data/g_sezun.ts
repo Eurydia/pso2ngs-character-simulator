8b0d9cb6c301fe3c3349e9170ca034476616cfb9
Euryn <crossdeltavi@gmail.com>
2023-01-06 10:15:11 +0100

    Update g_sezun.ts

--- src/assets/augments/data/g_sezun.ts
+++ src/assets/augments/data/g_sezun.ts
@@ -8,41 +8,51 @@ export const G_SEZUN: Augment[] = [];
 const makeAugmentSezun = (
   name: string,
   level: number,
-  getStatObject: (ctx: ActionContext) => StatObject,
+  getAwareStatObject: (ctx: ActionContext) => StatObject,
 ): Augment => {
   return augment(
     name,
     level,
     GroupEnumAugment.SEZUN,
     [GroupEnumAugment.SEZUN],
-    getStatObject,
+    getAwareStatObject,
   );
 };
 
 // --------------------------------------
 
 (() => {
-  const _getStatObject = (ctx: ActionContext): StatObject => {
-    const stat: StatObject = statObject({ [StatEnum.CORE_BP]: 10 });
+  const _getter = (ctx: ActionContext): StatObject => {
+    let stat: StatObject = statObject({ [StatEnum.CORE_BP]: 10 });
 
     if (ctx.time === undefined) {
       return stat;
     }
 
     if (ctx.time.isDuringSezunEvent) {
-      stat.setStat(StatEnum.WEAPON_MELEE, 1.15);
-      stat.setStat(StatEnum.WEAPON_RANGED, 1.15);
-      stat.setStat(StatEnum.WEAPON_TECHNIQUE, 1.15);
-      stat.setStat(StatEnum.ADV_OFF_CRIT_CHANCE, 0.2);
-      stat.setStat(StatEnum.ADV_DEF_DAMAGE_RES, 1.25);
+      stat = StatObject.setStat(stat, StatEnum.WEAPON_MELEE, 1.15);
+      stat = StatObject.setStat(stat, StatEnum.WEAPON_RANGED, 1.15);
+      stat = StatObject.setStat(
+        stat,
+        StatEnum.WEAPON_TECHNIQUE,
+        1.15,
+      );
+      stat = StatObject.setStat(
+        stat,
+        StatEnum.ADV_OFF_CRIT_CHANCE,
+        0.2,
+      );
+      stat = StatObject.setStat(
+        stat,
+        StatEnum.ADV_DEF_DAMAGE_RES,
+        1.25,
+      );
     }
 
     return stat;
   };
 
-  G_SEZUN.push(makeAugmentSezun("Sezun Lunafiv", 0, _getStatObject));
-  G_SEZUN.push(
-    makeAugmentSezun("Sezun Automfevre", 0, _getStatObject),
-  );
-  G_SEZUN.push(makeAugmentSezun("Sezun Wintafiv", 0, _getStatObject));
+  G_SEZUN.push(makeAugmentSezun("Sezun Lunafiv", 0, _getter));
+  G_SEZUN.push(makeAugmentSezun("Sezun Automfevre", 0, _getter));
+  G_SEZUN.push(makeAugmentSezun("Sezun Wintafiv", 0, _getter));
 })();
