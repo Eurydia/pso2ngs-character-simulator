313b0374204b2eea5def29da20652619de9d056d
Euryn <crossdeltavi@gmail.com>
2023-01-06 09:52:28 +0100

    Update g_addi.ts

--- src/assets/augments/data/g_addi.ts
+++ src/assets/augments/data/g_addi.ts
@@ -34,70 +34,94 @@ const makeAugmentAddi = (
     const [suffix, stat_weapon_up] = entry;
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Deft${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 10,
-          [stat_weapon_up]: WEAPON_UP,
-          [StatEnum.ADV_OFF_FLOOR]: 1.025,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Deft${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 10,
+            [stat_weapon_up]: WEAPON_UP,
+            [StatEnum.ADV_OFF_FLOOR]: 1.025,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Gua${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 10,
-          [stat_weapon_up]: WEAPON_UP,
-          [StatEnum.ADV_DEF_DAMAGE_RES]: 1.025,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Gua${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 10,
+            [stat_weapon_up]: WEAPON_UP,
+            [StatEnum.ADV_DEF_DAMAGE_RES]: 1.025,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Spi${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 10,
-          [StatEnum.CORE_PP]: 6,
-          [stat_weapon_up]: WEAPON_UP,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Spi${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 10,
+            [StatEnum.CORE_PP]: 6,
+            [stat_weapon_up]: WEAPON_UP,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Sta${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 10,
-          [StatEnum.CORE_HP]: 20,
-          [stat_weapon_up]: WEAPON_UP,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Sta${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 10,
+            [StatEnum.CORE_HP]: 20,
+            [stat_weapon_up]: WEAPON_UP,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Staspi${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 10,
-          [StatEnum.CORE_HP]: 10,
-          [StatEnum.CORE_PP]: 3,
-          [stat_weapon_up]: WEAPON_UP,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Staspi${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 10,
+            [StatEnum.CORE_HP]: 10,
+            [StatEnum.CORE_PP]: 3,
+            [stat_weapon_up]: WEAPON_UP,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Ward${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 10,
-          [stat_weapon_up]: WEAPON_UP,
-          [StatEnum.AIL_BLIND]: 1.2,
-          [StatEnum.AIL_BURN]: 1.2,
-          [StatEnum.AIL_FREEZE]: 1.2,
-          [StatEnum.AIL_PANIC]: 1.2,
-          [StatEnum.AIL_DOWN]: 1.2,
-          [StatEnum.AIL_POISON]: 1.2,
-          [StatEnum.AIL_SHOCK]: 1.2,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Ward${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 10,
+            [stat_weapon_up]: WEAPON_UP,
+            [StatEnum.AIL_BLIND]: 1.2,
+            [StatEnum.AIL_BURN]: 1.2,
+            [StatEnum.AIL_FREEZE]: 1.2,
+            [StatEnum.AIL_PANIC]: 1.2,
+            [StatEnum.AIL_DOWN]: 1.2,
+            [StatEnum.AIL_POISON]: 1.2,
+            [StatEnum.AIL_SHOCK]: 1.2,
+          });
+        },
+      ),
     );
   }
 })();
@@ -117,81 +141,105 @@ const makeAugmentAddi = (
     const [suffix, [stat_weapon_up_a, stat_weapon_up_b]] = entry;
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Deft${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 12,
-          [stat_weapon_up_a]: WEAPON_UP,
-          [stat_weapon_up_b]: WEAPON_UP,
-          [StatEnum.ADV_OFF_FLOOR]: 1.0275,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Deft${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 12,
+            [stat_weapon_up_a]: WEAPON_UP,
+            [stat_weapon_up_b]: WEAPON_UP,
+            [StatEnum.ADV_OFF_FLOOR]: 1.0275,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Gua${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 12,
-          [stat_weapon_up_a]: WEAPON_UP,
-          [stat_weapon_up_b]: WEAPON_UP,
-          [StatEnum.ADV_DEF_DAMAGE_RES]: 1.025,
-          [StatEnum.ADV_OFF_FLOOR]: 1.01,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Gua${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 12,
+            [stat_weapon_up_a]: WEAPON_UP,
+            [stat_weapon_up_b]: WEAPON_UP,
+            [StatEnum.ADV_DEF_DAMAGE_RES]: 1.025,
+            [StatEnum.ADV_OFF_FLOOR]: 1.01,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Spi${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 12,
-          [StatEnum.CORE_PP]: 6,
-          [stat_weapon_up_a]: WEAPON_UP,
-          [stat_weapon_up_b]: WEAPON_UP,
-          [StatEnum.ADV_OFF_FLOOR]: 1.01,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Spi${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 12,
+            [StatEnum.CORE_PP]: 6,
+            [stat_weapon_up_a]: WEAPON_UP,
+            [stat_weapon_up_b]: WEAPON_UP,
+            [StatEnum.ADV_OFF_FLOOR]: 1.01,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Sta${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 12,
-          [StatEnum.CORE_HP]: 20,
-          [stat_weapon_up_a]: WEAPON_UP,
-          [stat_weapon_up_b]: WEAPON_UP,
-          [StatEnum.ADV_OFF_FLOOR]: 1.01,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Sta${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 12,
+            [StatEnum.CORE_HP]: 20,
+            [stat_weapon_up_a]: WEAPON_UP,
+            [stat_weapon_up_b]: WEAPON_UP,
+            [StatEnum.ADV_OFF_FLOOR]: 1.01,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Staspi${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 12,
-          [StatEnum.CORE_HP]: 10,
-          [StatEnum.CORE_PP]: 3,
-          [stat_weapon_up_a]: WEAPON_UP,
-          [stat_weapon_up_b]: WEAPON_UP,
-          [StatEnum.ADV_OFF_FLOOR]: 1.01,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Staspi${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 12,
+            [StatEnum.CORE_HP]: 10,
+            [StatEnum.CORE_PP]: 3,
+            [stat_weapon_up_a]: WEAPON_UP,
+            [stat_weapon_up_b]: WEAPON_UP,
+            [StatEnum.ADV_OFF_FLOOR]: 1.01,
+          });
+        },
+      ),
     );
 
     G_ADDI.push(
-      makeAugmentAddi(`Addi Ward${suffix}`, 0, (_): StatObject => {
-        return statObject({
-          [StatEnum.CORE_BP]: 12,
-          [stat_weapon_up_a]: WEAPON_UP,
-          [stat_weapon_up_b]: WEAPON_UP,
-          [StatEnum.AIL_BLIND]: 1.2,
-          [StatEnum.AIL_BURN]: 1.2,
-          [StatEnum.AIL_FREEZE]: 1.2,
-          [StatEnum.AIL_PANIC]: 1.2,
-          [StatEnum.AIL_DOWN]: 1.2,
-          [StatEnum.AIL_POISON]: 1.2,
-          [StatEnum.AIL_SHOCK]: 1.2,
-          [StatEnum.ADV_OFF_FLOOR]: 1.01,
-        });
-      }),
+      makeAugmentAddi(
+        `Addi Ward${suffix}`,
+        0,
+        (_: ActionContext): StatObject => {
+          return statObject({
+            [StatEnum.CORE_BP]: 12,
+            [stat_weapon_up_a]: WEAPON_UP,
+            [stat_weapon_up_b]: WEAPON_UP,
+            [StatEnum.AIL_BLIND]: 1.2,
+            [StatEnum.AIL_BURN]: 1.2,
+            [StatEnum.AIL_FREEZE]: 1.2,
+            [StatEnum.AIL_PANIC]: 1.2,
+            [StatEnum.AIL_DOWN]: 1.2,
+            [StatEnum.AIL_POISON]: 1.2,
+            [StatEnum.AIL_SHOCK]: 1.2,
+            [StatEnum.ADV_OFF_FLOOR]: 1.01,
+          });
+        },
+      ),
     );
   }
 })();
