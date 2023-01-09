f97526b205f6f172638cbeaec09420e2db3e6e53
Euryn <crossdeltavi@gmail.com>
2023-01-09 08:21:09 +0100

    Update g_sezun.ts

--- src/assets/augments/data/g_sezun.ts
+++ src/assets/augments/data/g_sezun.ts
@@ -23,33 +23,18 @@ const makeAugmentSezun = (
 
 (() => {
   const _getter = (ctx: ActionContext): StatObject => {
-    let stat: StatObject = statObject({ [StatEnum.CORE_BP]: 10 });
-
-    if (ctx.time === undefined) {
+    const stat: StatObject = statObject({ [StatEnum.CORE_BP]: 10 });
+    if (!ctx.time.isDuringSezunEvent) {
       return stat;
     }
-
-    if (ctx.time.isDuringSezunEvent) {
-      stat = StatObject.setStat(stat, StatEnum.WEAPON_MELEE, 1.15);
-      stat = StatObject.setStat(stat, StatEnum.WEAPON_RANGED, 1.15);
-      stat = StatObject.setStat(
-        stat,
-        StatEnum.WEAPON_TECHNIQUE,
-        1.15,
-      );
-      stat = StatObject.setStat(
-        stat,
-        StatEnum.ADV_OFF_CRIT_CHANCE,
-        0.2,
-      );
-      stat = StatObject.setStat(
-        stat,
-        StatEnum.ADV_DEF_DAMAGE_RES,
-        1.25,
-      );
-    }
-
-    return stat;
+    const stat_up: StatObject = statObject({
+      [StatEnum.WEAPON_MELEE]: 1.15,
+      [StatEnum.WEAPON_RANGED]: 1.15,
+      [StatEnum.WEAPON_TECHNIQUE]: 1.15,
+      [StatEnum.ADV_OFF_CRIT_CHANCE]: 0.2,
+      [StatEnum.ADV_DEF_DAMAGE_RES]: 1.25,
+    });
+    return StatObject.merge(stat, stat_up);
   };
 
   G_SEZUN.push(makeAugmentSezun("Sezun Lunafiv", 0, _getter));
