286b4b8892badd775e53a8ed7db9b2b85d4fbef2
Euryn <crossdeltavi@gmail.com>
2023-01-09 19:53:34 +0100

    Update g_techter.ts

--- src/assets/addon_skills/data/g_techter.ts
+++ src/assets/addon_skills/data/g_techter.ts
@@ -4,7 +4,7 @@ import { StatEnum, statObject, StatObject } from "../../stat";
 import { addonSkill } from "../addon_skill";
 
 export const TECHTER_NATURAL_PB_RECOVERY = addonSkill(
-  "Natural PB Gauge Accumulation Up",
+  "Natural PB Gauge Accumulation Up (Techter)",
   (_: ActionContext, level_index: number): StatObject => {
     const DATA_PB_RECOVERY: number[] = [
       1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09, 1.1, 1.11,
@@ -21,7 +21,7 @@ export const TECHTER_NATURAL_PB_RECOVERY = addonSkill(
 );
 
 export const TECHTER_DAMAGE_RES_UP = addonSkill(
-  "Damage Resistance Up",
+  "Damage Resistance Up (Techter)",
   (_: ActionContext, level_index: number): StatObject => {
     const DATA_DAMAGE_RES: number[] = [
       1.005, 1.01, 1.015, 1.02, 1.025, 1.03, 1.035, 1.04, 1.045, 1.05,
@@ -38,7 +38,7 @@ export const TECHTER_DAMAGE_RES_UP = addonSkill(
 );
 
 export const TECHTER_POISON_RES_UP = addonSkill(
-  "Poison Resistance Up",
+  "Poison Resistance Up (Techter)",
   (_: ActionContext, level_index: number): StatObject => {
     const DATA_AIL_RES: number[] = [
       1.3, 1.32, 1.33, 1.34, 1.35, 1.36, 1.37, 1.38, 1.39, 1.4, 1.41,
