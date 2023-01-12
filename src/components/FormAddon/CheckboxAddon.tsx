8c6e98d3843009db3675bd8522d24b786ff699a3
Euryn <crossdeltavi@gmail.com>
2023-01-12 17:48:16 +0100

    Update CheckboxAddon.tsx

--- src/components/FormAddon/CheckboxAddon.tsx
+++ src/components/FormAddon/CheckboxAddon.tsx
@@ -4,27 +4,32 @@ import {
   LooksTwoRounded,
 } from "@mui/icons-material";
 import { Checkbox } from "@mui/material";
-import { FC, ReactNode } from "react";
+import { FC, memo, ReactNode } from "react";
 
 type CheckboxAddonProps = {
   orderNumber: number;
   onClick: () => void;
 };
-export const CheckboxAddon: FC<CheckboxAddonProps> = (props) => {
-  const { orderNumber, onClick } = props;
-  const checked: boolean = orderNumber > 0;
+export const CheckboxAddon: FC<CheckboxAddonProps> = memo(
+  (props) => {
+    const { orderNumber, onClick } = props;
+    const checked: boolean = orderNumber > 0;
 
-  let checked_icon: ReactNode = <LooksOneRounded />;
-  if (orderNumber === 2) {
-    checked_icon = <LooksTwoRounded />;
-  }
+    let checked_icon: ReactNode = <LooksOneRounded />;
+    if (orderNumber === 2) {
+      checked_icon = <LooksTwoRounded />;
+    }
 
-  return (
-    <Checkbox
-      icon={<CheckBoxOutlineBlankRounded />}
-      checkedIcon={checked_icon}
-      checked={checked}
-      onClick={onClick}
-    />
-  );
-};
+    return (
+      <Checkbox
+        icon={<CheckBoxOutlineBlankRounded />}
+        checkedIcon={checked_icon}
+        checked={checked}
+        onClick={onClick}
+      />
+    );
+  },
+  (prev, next) => {
+    return prev.orderNumber === next.orderNumber;
+  },
+);
