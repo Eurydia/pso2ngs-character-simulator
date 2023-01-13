1b97f3153fb81b41fa920be2c9274768efd81b0b
Euryn <crossdeltavi@gmail.com>
2023-01-13 13:13:23 +0100

    Update FormUnit.tsx

--- src/components/FormUnit/FormUnit.tsx
+++ src/components/FormUnit/FormUnit.tsx
@@ -1,4 +1,11 @@
-import { Fragment, FC, useState, memo } from "react";
+import {
+  Fragment,
+  FC,
+  useState,
+  memo,
+  useCallback,
+  useMemo,
+} from "react";
 import {
   Dialog,
   DialogContent,
@@ -28,154 +35,178 @@ import { AutocompleteAugment } from "../AutocompleteAugment";
 import { StatView } from "../StatView";
 
 type FormUnitProps = {
-  cardTitle: string;
+  // Dynamic props
   stat: StatObject;
   formData: DataUnit;
-  onFormDataChange: (getter: (prev: DataUnit) => DataUnit) => void;
+
+  // Static props
+  cardTitle: string;
+
+  onUnitChange: (next_unit: Unit | null) => void;
+  onUnitLevelChange: (next_level: number) => void;
+  onFixaChange: (next_fixa: Fixa | null) => void;
+  onAugmentChange: (
+    next_augment: Augment | null,
+    augment_index: number,
+  ) => void;
+
   onSync: () => void;
 };
-export const FormUnit: FC<FormUnitProps> = memo((props) => {
-  const { stat, cardTitle, formData, onFormDataChange, onSync } =
-    props;
+export const FormUnit: FC<FormUnitProps> = memo(
+  (props) => {
+    const {
+      stat,
+      cardTitle,
+      formData,
+      onUnitChange,
+      onUnitLevelChange,
+      onFixaChange,
+      onAugmentChange,
+      onSync,
+    } = props;
 
-  const { unit, unit_level, fixa, augments } = formData;
+    const { unit, unit_level, fixa, augments } = formData;
 
-  const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialogOpen, setDialogOpen] = useState(false);
 
-  const handleDialogOpen = () => {
-    setDialogOpen(true);
-  };
-  const handleDialogClose = () => {
-    setDialogOpen(false);
-  };
-  const handleUnitChange = (value: Unit | null) => {
-    onFormDataChange((prev) => {
-      const next = { ...prev };
-      next.unit = value;
-      return next;
-    });
-  };
-  const handleUnitLevelChange = (value: number) => {
-    onFormDataChange((prev) => {
-      const next = { ...prev };
-      next.unit_level = value;
-      return next;
-    });
-  };
-  const handleFixaChange = (value: Fixa | null) => {
-    onFormDataChange((prev) => {
-      const next = { ...prev };
-      next.fixa = value;
-      return next;
-    });
-  };
-  const handleAugmentChange = (
-    value: Augment | null,
-    index: number,
-  ) => {
-    onFormDataChange((prev) => {
-      const next = { ...prev };
-      next.augments[index] = value;
-      next.augments = Augment.removeConflict(next.augments, index);
-      return next;
-    });
-  };
+    const handleDialogOpen = useCallback(() => {
+      setDialogOpen(true);
+    }, []);
 
-  let active_augments: (Augment | null)[] = [];
-  if (unit !== null) {
-    const active_count: number = Augment.getAugmentSlot(unit_level);
-    active_augments = augments.slice(0, active_count);
-  }
+    const handleDialogClose = useCallback(() => {
+      setDialogOpen(false);
+    }, []);
 
-  return (
-    <Fragment>
-      <FormBase
-        cardTitle={cardTitle}
-        slotCardHeaderAction={
-          <Fragment>
-            <Tooltip
-              placement="top"
-              title={<Typography>Sync with me</Typography>}
-            >
-              <IconButton
-                color="primary"
-                size="large"
-                onClick={onSync}
+    const active_augments = useMemo((): (Augment | null)[] => {
+      if (unit === null) {
+        return [];
+      }
+      const count: number = Augment.getAugmentSlot(unit_level);
+      return augments.slice(0, count);
+    }, [unit, augments, unit_level]);
+
+    return (
+      <Fragment>
+        <FormBase
+          cardTitle={cardTitle}
+          slotCardHeaderAction={
+            <Fragment>
+              <Tooltip
+                placement="top"
+                title={<Typography>Sync with me</Typography>}
               >
-                <SyncRounded />
-              </IconButton>
-            </Tooltip>
-            <Tooltip
-              placement="top"
-              title={<Typography>Open stat</Typography>}
-            >
-              <IconButton
-                color="primary"
-                size="large"
-                onClick={handleDialogOpen}
+                <IconButton
+                  color="primary"
+                  size="large"
+                  onClick={onSync}
+                >
+                  <SyncRounded />
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                placement="top"
+                title={<Typography>Open stat</Typography>}
               >
-                <BarChartRounded />
-              </IconButton>
-            </Tooltip>
-          </Fragment>
-        }
-        slotCardContent={
-          <Grid container spacing={2} columns={{ xs: 1, sm: 2 }}>
-            <Grid item xs={1}>
-              <Stack spacing={1}>
-                <AutocompleteUnit
-                  unit={unit}
-                  onUnitChange={handleUnitChange}
-                />
-                <FieldLevel
-                  label="Enhacement"
-                  disabled={unit === null}
-                  levelMin={0}
-                  levelMax={unit === null ? 0 : unit.enhancement_max}
-                  level={unit_level}
-                  onLevelChange={handleUnitLevelChange}
-                />
-                <AutocompleteFixa
-                  disabled={unit === null}
-                  fixa={fixa}
-                  onFixaChange={handleFixaChange}
-                  mode={GroupEnumFixa.UNIT}
-                />
-              </Stack>
+                <IconButton
+                  color="primary"
+                  size="large"
+                  onClick={handleDialogOpen}
+                >
+                  <BarChartRounded />
+                </IconButton>
+              </Tooltip>
+            </Fragment>
+          }
+          slotCardContent={
+            <Grid container spacing={2} columns={{ xs: 1, sm: 2 }}>
+              <Grid item xs={1}>
+                <Stack spacing={1}>
+                  <AutocompleteUnit
+                    unit={unit}
+                    onUnitChange={onUnitChange}
+                  />
+                  <FieldLevel
+                    label="Enhacement"
+                    levelMin={0}
+                    disabled={unit === null}
+                    levelMax={
+                      unit === null ? 0 : unit.enhancement_max
+                    }
+                    level={unit_level}
+                    onLevelChange={onUnitLevelChange}
+                  />
+                  <AutocompleteFixa
+                    mode={GroupEnumFixa.UNIT}
+                    disabled={unit === null}
+                    fixa={fixa}
+                    onFixaChange={onFixaChange}
+                  />
+                </Stack>
+              </Grid>
+              <Grid item xs={1}>
+                <Stack spacing={1}>
+                  {augments.map((augment, augment_index) => {
+                    return (
+                      <AutocompleteAugment
+                        key={`augment-${augment_index}`}
+                        disabled={
+                          unit === null ||
+                          augment_index >= active_augments.length
+                        }
+                        augment={augment}
+                        onAugmentChange={(next_augment) => {
+                          onAugmentChange(
+                            next_augment,
+                            augment_index,
+                          );
+                        }}
+                      />
+                    );
+                  })}
+                </Stack>
+              </Grid>
             </Grid>
-            <Grid item xs={1}>
-              <Stack spacing={1}>
-                {augments.map((augment, index) => {
-                  const disabled =
-                    unit === null || index >= active_augments.length;
-                  return (
-                    <AutocompleteAugment
-                      key={`augment-${index}`}
-                      disabled={disabled}
-                      augment={augment}
-                      onAugmentChange={(value) => {
-                        handleAugmentChange(value, index);
-                      }}
-                    />
-                  );
-                })}
-              </Stack>
-            </Grid>
-          </Grid>
-        }
-      />
-      <Dialog
-        keepMounted
-        fullWidth
-        maxWidth="sm"
-        open={dialogOpen}
-        onClose={handleDialogClose}
-      >
-        <DialogTitle>{`${cardTitle} summary`}</DialogTitle>
-        <DialogContent>
-          <StatView stat={stat} maxHeight="" />
-        </DialogContent>
-      </Dialog>
-    </Fragment>
-  );
-});
+          }
+        />
+        <Dialog
+          keepMounted
+          fullWidth
+          maxWidth="sm"
+          open={dialogOpen}
+          onClose={handleDialogClose}
+        >
+          <DialogTitle>{`${cardTitle} summary`}</DialogTitle>
+          <DialogContent>
+            <StatView stat={stat} maxHeight="" />
+          </DialogContent>
+        </Dialog>
+      </Fragment>
+    );
+  },
+  (prev, next) => {
+    const prevFormData = prev.formData;
+    const nextFormData = next.formData;
+    if (prevFormData.unit?.label !== nextFormData.unit?.label) {
+      return false;
+    }
+    if (prevFormData.unit_level !== nextFormData.unit_level) {
+      return false;
+    }
+    if (prevFormData.fixa?.label !== nextFormData.fixa?.label) {
+      return false;
+    }
+
+    const prevAugments = prevFormData.augments;
+    const nextAugments = nextFormData.augments;
+    if (prevAugments.length !== nextAugments.length) {
+      return false;
+    }
+    for (let i = 0; i < prevAugments.length; i++) {
+      if (prevAugments[i]?.label !== nextAugments[i]?.label) {
+        return false;
+      }
+    }
+
+    return true;
+  },
+);
