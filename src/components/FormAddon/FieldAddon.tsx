9f2e60f8361012baeba8635423ccd55dfca9cdc7
Euryn <crossdeltavi@gmail.com>
2023-01-13 12:46:16 +0100

    Update FieldAddon.tsx

--- src/components/FormAddon/FieldAddon.tsx
+++ src/components/FormAddon/FieldAddon.tsx
@@ -5,40 +5,37 @@ import { FieldLevel } from "../FieldLevel";
 
 type FieldAddonProps = {
   slotCheckbox: ReactNode;
+  level: number;
+
   bold: boolean;
   title: string;
-  level: number;
+
   onLevelChange: (value: number) => void;
 };
-export const FieldAddon: FC<FieldAddonProps> = memo(
-  (props) => {
-    const { title, bold, slotCheckbox, level, onLevelChange } = props;
-    return (
-      <Box>
-        <Grid container columns={3} spacing={1} alignItems="center">
-          <Grid item xs>
-            <Stack spacing={1} direction="row" alignItems="center">
-              {slotCheckbox}
-              <Typography fontWeight={bold ? "bold" : "normal"}>
-                {title}
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid item xs={1}>
-            <FieldLevel
-              disabled={false}
-              label="Level"
-              levelMin={0}
-              levelMax={AddonSkill.LEVEL_MAX}
-              level={level}
-              onLevelChange={onLevelChange}
-            />
-          </Grid>
+export const FieldAddon: FC<FieldAddonProps> = (props) => {
+  const { title, bold, slotCheckbox, level, onLevelChange } = props;
+  return (
+    <Box>
+      <Grid container columns={3} spacing={1} alignItems="center">
+        <Grid item xs>
+          <Stack spacing={1} direction="row" alignItems="center">
+            {slotCheckbox}
+            <Typography fontWeight={bold ? "bold" : "normal"}>
+              {title}
+            </Typography>
+          </Stack>
         </Grid>
-      </Box>
-    );
-  },
-  (prev, next) => {
-    return prev.level === next.level;
-  },
-);
+        <Grid item xs={1}>
+          <FieldLevel
+            disabled={false}
+            label="Level"
+            levelMin={0}
+            levelMax={AddonSkill.LEVEL_MAX}
+            level={level}
+            onLevelChange={onLevelChange}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
