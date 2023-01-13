97313b2206bbcb07cf76ac77c5937508967697ee
Euryn <crossdeltavi@gmail.com>
2023-01-13 20:47:40 +0100

    Update FormAddon.tsx

--- src/components/FormAddon/FormAddon.tsx
+++ src/components/FormAddon/FormAddon.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment, memo, useCallback, useState } from "react";
+import { FC, Fragment, useCallback, useState } from "react";
 import {
   Tooltip,
   Typography,
@@ -34,126 +34,95 @@ type FormAddonProps = {
   onSubLevelChange: (next_level: number, skill_index: number) => void;
   onSubActiveIndexChange: (skill_index: number) => void;
 };
-export const FormAddon: FC<FormAddonProps> = memo(
-  (props) => {
-    const {
-      title,
-      stat,
-      mainLabel,
-      subLabels,
-      mainLevel,
-      subLevels,
-      subActiveIndexes,
-      onMainLevelChange,
-      onSubLevelChange,
-      onSubActiveIndexChange,
-    } = props;
+export const FormAddon: FC<FormAddonProps> = (props) => {
+  const {
+    title,
+    stat,
+    mainLabel,
+    subLabels,
+    mainLevel,
+    subLevels,
+    subActiveIndexes,
+    onMainLevelChange,
+    onSubLevelChange,
+    onSubActiveIndexChange,
+  } = props;
 
-    const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
 
-    const handleDialogOpen = useCallback(() => {
-      setDialogOpen(true);
-    }, []);
-    const handleDialogClose = useCallback(() => {
-      setDialogOpen(false);
-    }, []);
+  const handleDialogOpen = useCallback(() => {
+    setDialogOpen(true);
+  }, []);
+  const handleDialogClose = useCallback(() => {
+    setDialogOpen(false);
+  }, []);
 
-    return (
-      <Fragment>
-        <FormBase
-          cardTitle={title}
-          slotCardHeaderAction={
-            <Tooltip
-              placement="top"
-              title={<Typography>Open summary</Typography>}
+  return (
+    <Fragment>
+      <FormBase
+        cardTitle={title}
+        slotCardHeaderAction={
+          <Tooltip
+            placement="top"
+            title={<Typography>Open summary</Typography>}
+          >
+            <IconButton
+              size="large"
+              color="primary"
+              onClick={handleDialogOpen}
             >
-              <IconButton
-                size="large"
-                color="primary"
-                onClick={handleDialogOpen}
-              >
-                <BarChartRounded />
-              </IconButton>
-            </Tooltip>
-          }
-          slotCardContent={
-            <Stack spacing={3}>
-              <FieldAddon
-                bold
-                title={mainLabel}
-                slotCheckbox={null}
-                level={mainLevel}
-                onLevelChange={onMainLevelChange}
-              />
-              <Stack spacing={1}>
-                {subActiveIndexes.map((order_number, skill_index) => {
-                  const sub_level: number = subLevels[skill_index];
-                  const sub_label: string = subLabels[skill_index];
-                  return (
-                    <FieldAddon
-                      key={`${sub_label}-${skill_index}`}
-                      bold={false}
-                      title={sub_label}
-                      slotCheckbox={
-                        <CheckboxAddon
-                          orderNumber={order_number}
-                          onClick={() => {
-                            onSubActiveIndexChange(skill_index);
-                          }}
-                        />
-                      }
-                      level={sub_level}
-                      onLevelChange={(next_level: number) => {
-                        onSubLevelChange(next_level, skill_index);
-                      }}
-                    />
-                  );
-                })}
-              </Stack>
+              <BarChartRounded />
+            </IconButton>
+          </Tooltip>
+        }
+        slotCardContent={
+          <Stack spacing={3}>
+            <FieldAddon
+              bold
+              title={mainLabel}
+              slotCheckbox={null}
+              level={mainLevel}
+              onLevelChange={onMainLevelChange}
+            />
+            <Stack spacing={1}>
+              {subActiveIndexes.map((order_number, skill_index) => {
+                const sub_level: number = subLevels[skill_index];
+                const sub_label: string = subLabels[skill_index];
+                return (
+                  <FieldAddon
+                    key={`${sub_label}-${skill_index}`}
+                    bold={false}
+                    title={sub_label}
+                    slotCheckbox={
+                      <CheckboxAddon
+                        orderNumber={order_number}
+                        onClick={() => {
+                          onSubActiveIndexChange(skill_index);
+                        }}
+                      />
+                    }
+                    level={sub_level}
+                    onLevelChange={(next_level: number) => {
+                      onSubLevelChange(next_level, skill_index);
+                    }}
+                  />
+                );
+              })}
             </Stack>
-          }
-        />
-        <Dialog
-          fullWidth
-          maxWidth="sm"
-          open={dialogOpen}
-          onClose={handleDialogClose}
-        >
-          <DialogTitle>{`${title} addon summary`}</DialogTitle>
-          <DialogContent>
-            <StatView stat={stat} maxHeight="" />
-          </DialogContent>
-        </Dialog>
-      </Fragment>
-    );
-  },
-  (prev, next) => {
-    if (prev.mainLevel !== next.mainLevel) {
-      return false;
-    }
-
-    const prevSubLevels = prev.subLevels;
-    const nextSubLevels = next.subLevels;
-    if (prevSubLevels.length !== nextSubLevels.length) {
-      return false;
-    }
-    for (let i = 0; i < prevSubLevels.length; i++) {
-      if (prevSubLevels[i] !== nextSubLevels[i]) {
-        return false;
-      }
-    }
-
-    const prevSubActiveIndexes = prev.subActiveIndexes;
-    const nextSubActiveIndexes = next.subActiveIndexes;
-    if (prevSubActiveIndexes.length !== nextSubActiveIndexes.length) {
-      return false;
-    }
-    for (let i = 0; i < prevSubActiveIndexes.length; i++) {
-      if (prevSubActiveIndexes[i] !== nextSubActiveIndexes[i]) {
-        return false;
-      }
-    }
-
-    return true;
-  },
-);
+          </Stack>
+        }
+      />
+      <Dialog
+        fullWidth
+        maxWidth="sm"
+        open={dialogOpen}
+        onClose={handleDialogClose}
+      >
+        <DialogTitle>{`${title} addon summary`}</DialogTitle>
+        <DialogContent>
+          <StatView stat={stat} maxHeight="" />
+        </DialogContent>
+      </Dialog>
+    </Fragment>
+  );
+};
