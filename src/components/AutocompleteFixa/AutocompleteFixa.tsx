41c1d500b8d9231007b86563207d2fba18f6c0d0
Euryn <crossdeltavi@gmail.com>
2023-01-13 20:44:04 +0100

    Update AutocompleteFixa.tsx

--- src/components/AutocompleteFixa/AutocompleteFixa.tsx
+++ src/components/AutocompleteFixa/AutocompleteFixa.tsx
@@ -30,17 +30,20 @@ export const AutocompleteFixa: FC<AutocompleteFixaProps> = memo(
   (props) => {
     const { mode, disabled, fixa, onFixaChange } = props;
 
-    const handleFixaChange = (
-      event: SyntheticEvent<Element, Event>,
-      value: Fixa | null,
-      reason: AutocompleteChangeReason,
-    ): void => {
-      onFixaChange(value);
-    };
+    const handleFixaChange = useCallback(
+      (
+        event: SyntheticEvent<Element, Event>,
+        value: Fixa | null,
+        reason: AutocompleteChangeReason,
+      ): void => {
+        onFixaChange(value);
+      },
+      [],
+    );
 
     const options = useMemo((): Fixa[] => {
       return AssetFixas.filter((fixa) => {
-        return fixa.group === props.mode;
+        return fixa.group === mode;
       });
     }, [mode]);
 
