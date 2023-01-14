ae5c15afbb7c8d6c82d78577c7606e493f918a75
Euryn <crossdeltavi@gmail.com>
2023-01-14 08:54:17 +0100

    Update IconButtonTooltip.tsx

--- src/components/IconButtonTooltip.tsx
+++ src/components/IconButtonTooltip.tsx
@@ -1,21 +1,26 @@
 import { IconButton, Tooltip, Typography } from "@mui/material";
-import { FC, ReactNode } from "react";
+import { FC, memo, ReactNode } from "react";
 
 type IconButtonTooltipProps = {
   icon: ReactNode;
   tooltipText: string;
   onClick: () => void;
 };
-export const IconButtonTooltip: FC<IconButtonTooltipProps> = (
-  props,
-) => {
-  const { icon, tooltipText, onClick } = props;
-  return (
-    <Tooltip
-      placement="top"
-      title={<Typography>{tooltipText}</Typography>}
-    >
-      <IconButton onClick={onClick}>{icon}</IconButton>
-    </Tooltip>
-  );
-};
+export const IconButtonTooltip: FC<IconButtonTooltipProps> = memo(
+  (props) => {
+    const { icon, tooltipText, onClick } = props;
+    return (
+      <Tooltip
+        placement="top"
+        title={<Typography>{tooltipText}</Typography>}
+      >
+        <IconButton size="large" color="primary" onClick={onClick}>
+          {icon}
+        </IconButton>
+      </Tooltip>
+    );
+  },
+  (prev, next) => {
+    return true;
+  },
+);
