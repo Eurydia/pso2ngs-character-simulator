54776640270ed3094acc67361bf5b0969cf4e334
Euryn <crossdeltavi@gmail.com>
2023-01-14 10:34:00 +0100

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,8 +3,6 @@ export { useFixa } from "./useFixa";
 export { useWeapon } from "./useWeapon";
 export { useUnit } from "./useUnit";
 export { useEnhancement } from "./useEnhancement";
-// export { useFood } from "./useFood";
 
 export { useFormUnit } from "./useFormUnit";
 export { useActionContext } from "./useActionContext";
-// export { useFormAddon } from "./useFormAddon";
