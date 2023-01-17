ddc3ad1eaa89c2f16fdfecffe6cd129e83c1c63f
Euryn <crossdeltavi@gmail.com>
2023-01-17 18:58:22 +0100

    Update useWeapon.ts

--- src/hooks/useWeapon.ts
+++ src/hooks/useWeapon.ts
@@ -1,11 +1,9 @@
 import { useState, useEffect, useCallback } from "react";
 
 import { Weapon } from "../assets";
+import { useNumber } from "./useNumber";
 
 import { isValidJSON } from "./utility";
-
-const SUFFIX_KEY_WEAPON: string = "weapon";
-const SUFFIX_KEY_POTENTIAL_LEVEL: string = "potential-level";
 
 // ---------------------------------------------
 // Setter
@@ -13,23 +11,14 @@ const saveWeapon = (
   storage_key: string,
   weapon: Weapon | null,
 ): void => {
-  const KEY: string = `${storage_key}-${SUFFIX_KEY_WEAPON}`;
   const data_string: string = Weapon.toString(weapon);
-  localStorage.setItem(KEY, data_string);
-};
-const savePotentialLevel = (
-  storage_key: string,
-  potential_level: number,
-): void => {
-  const KEY: string = `${storage_key}-${SUFFIX_KEY_POTENTIAL_LEVEL}`;
-  const data_string: string = JSON.stringify(potential_level);
-  localStorage.setItem(KEY, data_string);
+  localStorage.setItem(storage_key, data_string);
 };
 // ---------------------------------------------
 // Getter
 const loadWeapon = (storage_key: string): Weapon | null => {
-  const KEY: string = `${storage_key}-${SUFFIX_KEY_WEAPON}`;
-  const loaded_string: string | null = localStorage.getItem(KEY);
+  const loaded_string: string | null =
+    localStorage.getItem(storage_key);
   if (loaded_string === null) {
     return null;
   }
@@ -42,21 +31,6 @@ const loadWeapon = (storage_key: string): Weapon | null => {
   }
   return Weapon.fromLabel(label);
 };
-export const loadPotentialLevel = (storage_key: string): number => {
-  const KEY: string = `${storage_key}-${SUFFIX_KEY_POTENTIAL_LEVEL}`;
-  const loaded_string: string | null = localStorage.getItem(KEY);
-  if (loaded_string === null) {
-    return 0;
-  }
-  if (!isValidJSON(loaded_string)) {
-    return 0;
-  }
-  const parsed_string: number = Number.parseInt(loaded_string);
-  if (Number.isNaN(parsed_string)) {
-    return 0;
-  }
-  return parsed_string;
-};
 // ---------------------------------------------
 // Hook
 export const useWeapon = (
@@ -67,21 +41,11 @@ export const useWeapon = (
   setWeapon: (next_weapon: Weapon | null) => void;
   setPotentialLevel: (next_level: number) => void;
 } => {
-  const [potentialLevel, _setPotentialLevel] = useState<number>(
-    () => {
-      return loadPotentialLevel(storage_key);
-    },
-  );
-  const [weapon, _setWeapon] = useState<Weapon | null>(() => {
+  const [weapon, _setWeapon] = useState((): Weapon | null => {
     return loadWeapon(storage_key);
   });
-
-  const setPotentialLevel = useCallback(
-    (next_level: number): void => {
-      _setPotentialLevel(next_level);
-    },
-    [],
-  );
+  const { value: potentialLevel, setValue: setPotentialLevel } =
+    useNumber(`${storage_key}-potential-level`);
 
   const setWeapon = useCallback(
     (next_weapon: Weapon | null): void => {
@@ -94,10 +58,6 @@ export const useWeapon = (
   useEffect(() => {
     saveWeapon(storage_key, weapon);
   }, [weapon]);
-
-  useEffect(() => {
-    savePotentialLevel(storage_key, potentialLevel);
-  }, [potentialLevel]);
 
   return { weapon, potentialLevel, setWeapon, setPotentialLevel };
 };
