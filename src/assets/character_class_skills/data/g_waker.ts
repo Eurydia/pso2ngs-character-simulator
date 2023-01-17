383ad8c4f7a15b7e6e0b95576b12a8281da07178
Euryn <crossdeltavi@gmail.com>
2023-01-17 10:27:52 +0100

    Create g_waker.ts

---
+++ src/assets/character_class_skills/data/g_waker.ts
@@ -0,0 +1,55 @@
+import { ActionContext } from "../../ContextAction";
+import { StatEnum, statObject, StatObject } from "../../stat";
+import {
+  CharacterClassSkill,
+  characterClassSkill,
+} from "../character_class_skill";
+
+export const G_WAKER_WELFARE_MANAGEMENT =
+  ((): CharacterClassSkill => {
+    const DATA_DAMAGE_RES_MAIN: number[] = [
+      1.2, 1.25, 1.3, 1.35, 1.4,
+    ];
+    const DATA_DAMAGE_RES_SUB: number[] = [
+      1.1, 1.13, 1.16, 1.18, 1.2,
+    ];
+    const DATA_HP_BREAKPOINT = [0.3, 0.3, 0.3, 0.3, 0.3];
+    const _getterMain = (
+      ctx: ActionContext,
+      level_index: number,
+    ): StatObject => {
+      const { hpValue, hpValueCurrent } = ctx.character;
+      const hp_percent: number = hpValueCurrent / hpValue;
+      const hp_breakpoint: number = DATA_HP_BREAKPOINT[level_index];
+      if (hp_percent > hp_breakpoint) {
+        return statObject();
+      }
+      const damage_res: number = DATA_DAMAGE_RES_MAIN[level_index];
+      return statObject({
+        [StatEnum.ADV_DEF_DAMAGE_RES]: damage_res,
+        [StatEnum.ADV_DEF_HEALING_UP]: 1.25,
+      });
+    };
+    const _getterSub = (
+      ctx: ActionContext,
+      level_index: number,
+    ): StatObject => {
+      const { hpValue, hpValueCurrent } = ctx.character;
+      const hp_percent: number = hpValueCurrent / hpValue;
+      const hp_breakpoint: number = DATA_HP_BREAKPOINT[level_index];
+      if (hp_percent > hp_breakpoint) {
+        return statObject();
+      }
+      const damage_res: number = DATA_DAMAGE_RES_SUB[level_index];
+      return statObject({
+        [StatEnum.ADV_DEF_DAMAGE_RES]: damage_res,
+        [StatEnum.ADV_DEF_HEALING_UP]: 1.1,
+      });
+    };
+    return characterClassSkill(
+      "Welfare Management",
+      DATA_DAMAGE_RES_MAIN.length,
+      _getterMain,
+      _getterSub,
+    );
+  })();
