62daf8f075fe1342818116a93eeaba0c6caac7e0
Euryn <crossdeltavi@gmail.com>
2023-01-17 08:16:24 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,11 @@
 export { StatView } from "./StatView";
+
 export { FormContextEditor } from "./FormContextEditor";
+
 export { FormWeapon } from "./FormWeapon";
 export { FormUnit } from "./FormUnit";
+
 export { FormFood } from "./FormFood";
-export { FormClassAddon } from "./FormClassAddon";
+
 export { FormClass } from "./FormClass";
+export { FormClassAddon } from "./FormClassAddon";
