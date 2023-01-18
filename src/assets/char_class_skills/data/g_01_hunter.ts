d6783db5a1ca65b95af781828013c4960de428af
Euryn <crossdeltavi@gmail.com>
2023-01-18 22:05:07 +0100

    Update g_01_hunter.ts

--- src/assets/char_class_skills/data/g_01_hunter.ts
+++ src/assets/char_class_skills/data/g_01_hunter.ts
@@ -5,8 +5,8 @@ import { CharClassSkill, charClassSkill } from "../char_class_skills";
 
 export const HUNTER_FLASH_GUARD = ((): CharClassSkill => {
   const DATA_DAMAGE_RES_MAIN: number[] = [
-    1.1, 1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.19, 1.2,
-    1.21, 1.22, 1.23, 1.24, 1.25,
+    1.1, 1.11, 1.12, 1.13, 1.14, 1.15, 1.16, 1.17, 1.18, 1.2, 1.21,
+    1.22, 1.23, 1.24, 1.25,
   ];
   const DATA_DAMAGE_RES_SUB: number[] = [
     1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09, 1.1, 1.11,
