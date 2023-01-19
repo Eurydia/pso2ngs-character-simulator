c6e04583ce40660dacc89133667aee80948a581f
Euryn <crossdeltavi@gmail.com>
2023-01-19 20:15:49 +0100

    Update helper.tsx

--- src/components/AutocompleteWeapon/helper.tsx
+++ src/components/AutocompleteWeapon/helper.tsx
@@ -6,13 +6,13 @@ import { Weapon } from "../../assets";
 const extractTerms = (value: string): string[] => {
   const items = value.split(" ");
   const terms: string[] = [];
-  for (const item of items) {
+  items.forEach((item) => {
     const item_trimmed = item.trim();
     if (item_trimmed === "") {
-      continue;
+      return;
     }
     terms.push(item_trimmed);
-  }
+  });
   return terms;
 };
 
@@ -32,13 +32,13 @@ const sieveOptions = (
 export const filterOptions = (
   options: Weapon[],
   state: FilterOptionsState<Weapon>,
-  size: number = 16,
+  result_size: number = 16,
 ) => {
   const value: string = state.inputValue;
   const terms: string[] = extractTerms(value);
   const filtered_options: Weapon[] = sieveOptions(
     options,
     terms,
-  ).slice(0, size);
+  ).slice(0, result_size);
   return filtered_options;
 };
