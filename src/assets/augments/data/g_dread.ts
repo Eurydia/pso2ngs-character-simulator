5e4f8e2f3c23d59ee12dbc3278c0328e76bd4df4
Euryn <crossdeltavi@gmail.com>
2023-02-06 14:18:05 +0100

    added search terms for dread augments

--- src/assets/augments/data/g_dread.ts
+++ src/assets/augments/data/g_dread.ts
@@ -9,6 +9,7 @@ const makeAugmentDread = (
   name: string,
   level: number,
   getAwareStatObject: (ctx: ActionContext) => StatObject,
+  searchable_terms: string[],
 ): Augment => {
   return Augment.create(
     name,
@@ -16,6 +17,7 @@ const makeAugmentDread = (
     GroupEnumAugment.DREAD,
     [GroupEnumAugment.DREAD],
     getAwareStatObject,
+    searchable_terms,
   );
 };
 
@@ -33,19 +35,25 @@ const makeAugmentDread = (
     const pp: number = DATA_PP[level_index];
     const floor_up: number = DATA_FLOOR_UP[level_index];
     const damage_res: number = DATA_DAMAGE_RES[level_index];
-    const _getter = (_: ActionContext): StatObject => {
-      return statObject({
-        [StatEnum.CORE_BP]: bp,
-        [StatEnum.CORE_HP]: hp,
-        [StatEnum.CORE_PP]: pp,
-        [StatEnum.ADV_OFF_FLOOR]: floor_up,
-        [StatEnum.ADV_DEF_DAMAGE_RES]: damage_res,
-      });
-    };
+
     const dread_augment: Augment = makeAugmentDread(
       "Dread Keeper",
       level,
-      _getter,
+      (_: ActionContext): StatObject => {
+        return statObject({
+          [StatEnum.CORE_BP]: bp,
+          [StatEnum.CORE_HP]: hp,
+          [StatEnum.CORE_PP]: pp,
+          [StatEnum.ADV_OFF_FLOOR]: floor_up,
+          [StatEnum.ADV_DEF_DAMAGE_RES]: damage_res,
+        });
+      },
+      [
+        StatEnum.CORE_HP,
+        StatEnum.CORE_PP,
+        StatEnum.ADV_OFF_FLOOR,
+        StatEnum.ADV_DEF_DAMAGE_RES,
+      ],
     );
     G_DREAD.push(dread_augment);
   });
